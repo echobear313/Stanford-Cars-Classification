32fadb300baaa3e9dfa62be31996d54195612a2b
Hzzone <Hzzone@outlook.com>
2017-11-07 22:03:53 +0800

    preprocess

--- python/preprocess.py
+++ python/preprocess.py
@@ -2,27 +2,66 @@ import cv2
 import util
 import numpy as np
 import os
+import logging
+import lmdb
+import sys
+sys.path.insert(0, "/home/hzzone/caffe/python")
+import caffe
 
-train_folder = ""
-test_folder = ""
+
+train_folder = "../data/cars_train"
+test_folder = "../data/cars_test"
 IMAGE_SIZE = 227
+PIXEL_MEAN = 0.40651412216
 
-def generate(source, mat_source):
-	data = util.loadmat(mat_source)
+logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S')
+
+def generate(source, mat_source, data_save_path, label_save_path):
+	data = util.loadmat(mat_source, "annotations")
+	labels = data[:, -2] - 1
 	counts = data.shape[0]
-	all_data = np.zeros((counts, IMAGE_SIZE, IMAGE_SIZE))
+	all_data = np.zeros((counts, 3, IMAGE_SIZE, IMAGE_SIZE))
 	for index in range(counts):
 		file_name = data[index][-1]
 		path = os.path.join(source, file_name)
 		im = cv2.imread(path)
-		x1 = data[index][1]
-		y1 = data[index][2]
-		x2 = data[index][3]
-		y2 = data[index][4]
+		x1 = data[index][0]
+		y1 = data[index][1]
+		x2 = data[index][2]
+		y2 = data[index][3]
 		im = im[y1:y2, x1:x2]
-		util.show(im)
-		exit()
+		im = cv2.resize(im, (IMAGE_SIZE, IMAGE_SIZE))
+		im = np.transpose(im, (2, 0, 1))
+		# all_data.astype(np.float)
+		all_data[index][...] = im*0.00390625 - PIXEL_MEAN
+		logging.debug("%s %s" % (index, path))
+	# data_label = np.array((all_data, labels))
+	np.save(data_save_path, all_data)
+	np.save(label_save_path, labels)
 
+def generate_lmdb(save_path, data_source, label_source, mat_source):
+	env = lmdb.Environment(save_path, map_size=int(1e12))
+	data = np.load(data_source)
+	labels = np.load(label_source)
+	print(labels.dtype)
+	print(data.dtype)
+	with env.begin(write=True) as txn:
+		for index in range(data.shape[0]):
+			datum = caffe.proto.caffe_pb2.Datum()
+			datum.channels = 3
+			datum.height = IMAGE_SIZE
+			datum.width = IMAGE_SIZE
+			datum.data = data[index].tobytes()
+			datum.label = labels[index]
+			str_id = "%8d" % index
+			txn.put(str_id, datum.SerializeToString())
+			logging.debug("%s %s" % (str_id, labels[index]))
+	logging.debug("%s complete" % save_path)
 
 if __name__ == "__main__":
-	generate()
+	# generate(train_folder, "../data/cars_train_annos.mat", "../data/train_data.npy", "../data/train_label.npy")
+	# generate(test_folder, "../data/cars_test_annos_withlabels.mat", "../data/test_data.npy", "../data/test_label.npy")
+	generate_lmdb("/home/hzzone/1tb/cars-data/train_lmdb", "../data/train_data.npy", "../data/train_label.npy", "../data/cars_train_annos.mat")
+	generate_lmdb("/home/hzzone/1tb/cars-data/test_lmdb", "../data/test_data.npy", "../data/test_label.npy", "../data/cars_test_annos_withlabels.mat")
--- python/util.py
+++ python/util.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import scipy.io as sio
 import numpy as np
+import preprocess
+from functools import reduce
 
 
 '''
@@ -22,10 +24,24 @@ def show(im):
 	plt.imshow(im)
 	plt.show()
 
+def calculate_mean():
+	train_data = preprocess.generate("../data/cars_train", "../data/cars_train_annos.mat")
+	train_shape = train_data.shape
+	train_sum = np.sum(train_data)
+	del train_data
+	test_data = preprocess.generate("../data/cars_test", "../data/cars_test_annos_withlabels.mat")
+	test_shape = test_data.shape
+	test_sum = np.sum(test_data)
+	del test_data
+	# data = np.vstack((train_data, test_data))
+	pixel_sum = train_sum + test_sum
+	pixel_counts = reduce(lambda x, y: x * y, test_shape) + reduce(lambda x, y: x * y, train_shape)
+	return pixel_sum/pixel_counts
 
 if __name__ == "__main__":
-	data = loadmat("/Users/HZzone/Desktop/Stanford-Cars-Classification/data/cars_train_annos.mat", "annotations")
-	print(data)
-
-	data = loadmat("/Users/HZzone/Desktop/Stanford-Cars-Classification/data/cars_test_annos_withlabels.mat", "annotations")
-	print(data)
+	# data = loadmat("/Users/HZzone/Desktop/Stanford-Cars-Classification/data/cars_train_annos.mat", "annotations")
+	# print(data)
+	#
+	# data = loadmat("/Users/HZzone/Desktop/Stanford-Cars-Classification/data/cars_test_annos_withlabels.mat", "annotations")
+	# print(data)
+	print(calculate_mean())
